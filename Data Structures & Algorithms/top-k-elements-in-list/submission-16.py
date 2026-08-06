da142e53d3b6630fb81dddbe2d74400dbc9e9947
jeremy87lee <157717264+jeremy87lee@users.noreply.github.com>
2026-08-06 11:53:41 -0400

    Add: top-k-elements-in-list - submission-16

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-16.py
@@ -0,0 +1,16 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        'MaxHeap and BucketSort'
+        hashMap = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            hashMap[n] = 1 + hashMap.get(n,0)
+        for n, h in hashMap.items():
+            freq[h].append(n)
+        res = []
+        for i in range(len(freq)-1,0,-1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
